cfb2e0f1b82727ea972955fcf6c9d4b04b83aea6
marumuji <marumuji@gmail.com>
2019-08-17 17:56:06 +0900

    Exhaustive Search

--- ALDS1_5_A.py
+++ ALDS1_5_A.py
@@ -1,5 +1,3 @@
-a = []
-n = 0
 def solve(i, m):
   if m == 0:
     return True
@@ -14,11 +12,11 @@ n = int(input())
 a = [int(i) for i in input().split()]
 q = int(input())
 m = [int(i) for i in input().split()]
-print(n, a, q, m)
 
-# m = 8 a = [1, 5, 7]
 for i in m:
-  if solve(0, i):
+  if sum(a) < i:
+    print("no")    
+  elif solve(0, i):
     print("yes")
   else:
     print("no")
